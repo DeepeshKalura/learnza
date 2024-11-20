9b01581026fa024b812807c7f569057656b520c6
DeepeshKalura <deepeshkalurs@gmail.com>
2024-11-20 08:11:26 +0530

    remove function going to create restapi from fastapi

--- functions/src/config.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from "firebase-functions";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export const config = {
-    mailsenderapi: process.env.MAIL_SENDER_API || functions.config().mailsenderapi.key,
-};
