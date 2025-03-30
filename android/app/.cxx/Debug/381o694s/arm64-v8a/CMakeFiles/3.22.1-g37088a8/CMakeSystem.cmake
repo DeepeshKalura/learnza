49ec737207e563f86f56244934b7e8425829ea08
DK <99230888+DeepeshKalura@users.noreply.github.com>
2025-03-30 05:53:12 +0530

    Deepesh kalura/issue89 (#90)
    * [Bug] A RenderFlex overflowed by 13 pixels on the bottom in Forgot Password Screen
    Fixes #89
    
    * [Bug] A RenderFlex overflowed by 13 pixels on the bottom in Forgot Password Screen
    
    add test things
    Fixes #89
    
    * [Bug] A RenderFlex overflowed by 13 pixels on the bottom in Forgot Password Screen
    
    add test case to handle the exception
    Fixes #89
    
    * feat: add mockito annotations for AuthProvider mock generation
    
    * test: remove unused mockito annotations and improve focus test in Forgot Password Screen
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- android/app/.cxx/Debug/381o694s/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/381o694s/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.8.0-53-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.8.0-56-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-53-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-56-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/deepesh/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake")
